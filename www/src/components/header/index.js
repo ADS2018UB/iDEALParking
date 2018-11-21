a8c8e68c317b1823ebae552e4721e7529d9ea5e3
FedericaSole <sole.1634218@studenti.uniroma1.it>
2018-11-21 19:30:44 +0100

    Added a preliminary form

--- www/src/components/header/index.js
+++ www/src/components/header/index.js
@@ -8,9 +8,11 @@ const Header = () => (
 		<nav>
 			<Link activeClassName={style.active} href="/">Home</Link>
 			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
+			<Link activeClassName={style.active} href="/profile/jonh">Jonh</Link>
 		</nav>
 	</header>
 );
+
+
 
 export default Header;
